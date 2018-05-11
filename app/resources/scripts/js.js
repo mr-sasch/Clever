71d70b2e914e82ca9a2fc560739088d43a6ac1ba
unknown <alex.m.sky@gmail.com>
2018-05-11 18:00:40 +0300

    reserve

--- app/resources/scripts/js.js
+++ app/resources/scripts/js.js
@@ -122,3 +122,92 @@ $(document).ready(function(){
     }, 500);
   });
 });
+
+// Таймер
+var timer;
+var thisis_time = $('#datefortimer')[0].value;
+  var compareDate = new Date(thisis_time+"T00:00:00");
+
+  timer = setInterval(function() {
+    timeBetweenDates(compareDate);
+  }, 1000);
+
+  function timeBetweenDates(toDate) {
+    var dateEntered = toDate;
+    var now = new Date();
+    var difference = dateEntered.getTime() - now.getTime();
+
+    if (difference <= 0) {
+
+      clearInterval(timer);
+
+    } else {
+
+      var seconds = Math.floor(difference / 1000);
+      var minutes = Math.floor(seconds / 60);
+      var hours = Math.floor(minutes / 60);
+      var days = Math.floor(hours / 24);
+
+      hours %= 24;
+      minutes %= 60;
+      seconds %= 60;
+
+      $(".days").text(days);
+      $(".hours").text(hours);
+      $(".minutes").text(minutes);
+      $(".seconds").text(seconds);
+    }
+  }
+
+// Инициация слайдера "Фото с производства"
+$('.photo-slider').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  variableWidth: true,
+  pauseOnHover: false,
+  swipeToSlide: true
+});
+baguetteBox.run('.photo-slider');
+
+// Отзывы
+var otzivi_slider = $('.otzivi-slider');
+
+otzivi_slider.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+    var i = (currentSlide ? currentSlide : 0) + 1;
+    $('.otzivi-slider__current').text(i);
+    $('.otzivi-slider__total').text(slick.slideCount);
+});
+
+otzivi_slider.slick({
+dots: false,
+infinite: true,
+speed: 300,
+slidesToShow: 3,
+slidesToScroll: 1,
+autoplay: true,
+autoplaySpeed: 3000,
+swipeToSlide:true,
+adaptiveHeight:true,
+responsive: [
+  {
+    breakpoint: 1170,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 1
+    }
+  },
+  {
+    breakpoint: 768,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false
+    }
+  }
+]
+});
